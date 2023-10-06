20273b8315111a08de0e758019c9866860e3e814
Jatin Arora <jatin.arora@canonical.com>
2023-10-06 16:26:24 +0530

    feat(charms): add nrpe relation to the charm
    This commit adds nrpe relation to the charm using the ops library  that
    we already have.

--- src/charm.py
+++ src/charm.py
@@ -6,23 +6,19 @@
 
 """Charmed Machine Operator For Anbox Application Registry (AAR)."""
 
-import ipaddress
 import logging
 import os
 import shutil
-from datetime import datetime, timedelta
 from pathlib import Path
 from subprocess import STDOUT, CalledProcessError, check_output
 
 import netifaces
 import ops
-from charms.operator_libs_linux.v1 import systemd
-from charms.operator_libs_linux.v2 import snap
-from cryptography import x509
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
+from nrpe.client import NRPEClient  # noqa: E402
+from lib.charms.operator_libs_linux.v2 import snap
+from helpers import pki
 from jinja2 import Environment, FileSystemLoader
+
 
 logger = logging.getLogger(__name__)
 
@@ -44,105 +40,15 @@ class AARCharm(ops.CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.stop, self._on_stop)
         self.framework.observe(self.on.client_relation_joined, self._on_aar_joined)
         self.framework.observe(self.on.publisher_relation_joined, self._on_aar_joined)
 
         self.framework.observe(self.on.client_relation_changed, self._on_aar_changed)
         self.framework.observe(self.on.publisher_relation_changed, self._on_aar_changed)
-
-    def _on_aar_joined(self, event: ops.RelationJoinedEvent):
-        with open(AAR_SERVER_CERT_PATH, "r") as f:
-            cert = f.read()
-        if not cert:
-            self.unit.status = ops.BlockedStatus("No registry certificate")
-            return
-
-        listen_address = self.private_ip
-        location = self.config.get("location")
-        if location:
-            listen_address = location
-
-        unit_data = event.relation.data[self.unit]
-        unit_data["certificate"] = cert
-        unit_data["fingerprint"] = self._get_fingerprint(cert)
-        unit_data["ip"] = listen_address
-        unit_data["port"] = str(self.config.get("port", ""))
-
-    def _get_fingerprint(self, cert: str) -> str:
-        x509_cert = x509.load_pem_x509_certificate(cert.encode())
-        return x509_cert.fingerprint(hashes.SHA256()).hex()
-
-    def _remove_all_certificates(self):
-        """Remove all old certificates held by AAR.
-
-        All client certificates are kept by juju in the relation until the hook
-        fails, so every time a new client is registered, the previous certificates
-        will be added again. To avoid duplicates and properly take into account
-        units that departed, we remove all certificates and add the ones that
-        are still active again.
-        """
-        shutil.rmtree(CLIENT_CERT_PATH)
-        shutil.rmtree(PUBLISHERS_CERT_PATH)
-        os.makedirs(CLIENT_CERT_PATH, 0o700, exist_ok=True)
-        os.makedirs(PUBLISHERS_CERT_PATH, 0o700, exist_ok=True)
-
-    def _on_aar_changed(self, event: ops.RelationChangedEvent):
-        self.unit.status = ops.MaintenanceStatus("Confguring new AMS Client")
-        self._remove_all_certificates()
-        ams_clients = []
-        for unit in event.relation.units:
-            cert = event.relation.data[unit].get("certificate")
-            if not cert:
-                continue
-
-            # If there is a relation mismatch, we set an error status
-            # ex: juju add-relation aar:client ams:publisher
-            mode = event.relation.data[unit]["mode"].strip('"')
-            if mode not in event.relation.name:
-                self.unit.status = ops.BlockedStatus(
-                    "Invalid relation {} to {}".format("arr-" + mode, event.relation.name)
-                )
-                return
-            ams_clients.append(event.relation.data[unit])
-
-        if not ams_clients:
-            return
-        for client in ams_clients:
-            self._register_ams_client(client["certificate"], event.relation.name)
-            logger.info(f"client registered {client['certificate']}")
-
-        aar_snap = snap.SnapCache()["aar"]
-        aar_snap.restart()
-        self.unit.status = ops.ActiveStatus()
-
-    def _register_ams_client(self, client_certificate: str, mode: str):
-        try:
-            client_certificate = client_certificate.replace("\\n", "\n").strip('"')
-            cmd = ["/snap/bin/aar", "trust", "add"]
-            if mode == "publisher":
-                cmd.append("--publisher")
-            check_output(cmd, stderr=STDOUT, input=client_certificate.encode("utf-8"))
-        except CalledProcessError as ex:
-            logger.error(f"failed to add client to aar: {ex.output}")
-            raise
-
-    def _snap_installed(self, snap_name) -> bool:
-        snap_client = snap.SnapClient()
-        snaps = snap_client.get_installed_snaps()
-        for installed_snap in snaps:
-            if installed_snap["name"] == snap_name:
-                return True
-        return False
-
-    def _get_ip_for_interface(self, interface):
-        """Return the ip address associated to the given interface."""
-        addresses = netifaces.ifaddresses(interface)
-        if netifaces.AF_INET not in addresses or "addr" not in addresses[netifaces.AF_INET][0]:
-            raise Exception("No IP associated to requested device")
-
-        return addresses[netifaces.AF_INET][0]["addr"]
+        self.nrpe_client = NRPEClient(self, "nrpe-external-master")
+        self.framework.observe(self.nrpe_client.on.nrpe_available, self._on_nrpe_available)
 
     @property
     def public_ip(self) -> str:
@@ -163,117 +69,57 @@ class AARCharm(ops.CharmBase):
         """Private address of the unit."""
         return self.model.get_binding("juju-info").network.bind_address.exploded
 
-    def _create_cert_files_if_needed(self, cert_path, key_path):
-        if os.path.exists(cert_path) and os.path.exists(key_path):
+    def _install_snap(self):
+        try:
+            res = self.model.resources.fetch("aar-snap")
+        except ops.ModelError:
+            res = None
+
+        # FIXME: Install the aar snap from a resource until we make the
+        # snaps in the snap store unlisted
+        if res is not None and res.stat().st_size:
+            snap.install_local(res, classic=False, dangerous=True)
+        else:
+            self.unit.status = ops.BlockedStatus("cannot install aar: snap resource not found")
             return
 
-        cert, key = self._generate_selfsigned_cert(self.public_ip, self.public_ip, self.private_ip)
-
-        cert_base_path = os.path.dirname(cert_path)
-        if not os.path.exists(cert_base_path):
-            os.makedirs(cert_base_path, mode=0o0700)
-
-        key_base_path = os.path.dirname(key_path)
-        if not os.path.exists(key_base_path):
-            os.makedirs(key_base_path, mode=0o0700)
-
-        with open(os.open(cert_path, os.O_CREAT | os.O_WRONLY, 0o600), "w") as f:
-            f.write(str(cert, "UTF-8"))
-
-        with open(os.open(key_path, os.O_CREAT | os.O_WRONLY, 0o600), "w") as f:
-            f.write(str(key, "UTF-8"))
-
-    def _generate_selfsigned_cert(self, hostname, public_ip, private_ip):
-        if not hostname:
-            raise Exception("A hostname is required")
-
-        if not public_ip:
-            raise Exception("A public IP is required")
-
-        if not private_ip:
-            raise Exception("A private IP is required")
-
-        key = rsa.generate_private_key(
-            public_exponent=65537, key_size=4096, backend=default_backend()
-        )
-
-        name = x509.Name([x509.NameAttribute(x509.NameOID.COMMON_NAME, hostname)])
-        alt_names = x509.SubjectAlternativeName(
-            [
-                # best practice seem to be to include the hostname in the SAN
-                x509.DNSName(hostname),
-                # allow addressing by IP, for when you don't have real DNS
-                # openssl wants DNSnames for ips...
-                x509.DNSName(public_ip),
-                x509.DNSName(private_ip),
-                x509.IPAddress(ipaddress.IPv4Address(public_ip)),
-                x509.IPAddress(ipaddress.IPv4Address(private_ip)),
-            ]
-        )
-        # path_len=0 means this cert can only sign itself, not other certs.
-        basic_contraints = x509.BasicConstraints(ca=True, path_length=0)
-        now = datetime.utcnow()
-        cert = (
-            x509.CertificateBuilder()
-            .subject_name(name)
-            .issuer_name(name)
-            .public_key(key.public_key())
-            .serial_number(1000)
-            .not_valid_before(now)
-            .not_valid_after(now + timedelta(days=10 * 365))
-            .add_extension(basic_contraints, False)
-            .add_extension(alt_names, False)
-            .sign(key, hashes.SHA256(), default_backend())
-        )
-        cert_pem = cert.public_bytes(encoding=serialization.Encoding.PEM)
-        key_pem = key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption(),
-        )
-
-        return cert_pem, key_pem
-
-    def _on_install(self, event: ops.InstallEvent):
-        if not self._snap_installed(SNAP_NAME):
-            self.unit.status = ops.MaintenanceStatus("installing aar")
-            try:
-                res = self.model.resources.fetch("aar-snap")
-            except ops.ModelError:
-                res = None
-            # FIXME: Install the aar snap from a resource until we make the
-            # snaps in the snap store unlisted
-            if res is not None and res.stat().st_size:
-                snap.install_local(res, classic=False, dangerous=True)
-            else:
-                self.unit.status = ops.BlockedStatus("cannot install aar: snap resource not found")
-                return
         aar_snap = snap.SnapCache()["aar"]
         aar_snap.connect(plug="home", slot=":home")
         aar_snap.connect(plug="network", slot=":network")
 
+    def _setup_certs(self):
         os.makedirs(CLIENT_CERT_PATH, 0o700, exist_ok=True)
         os.makedirs(PUBLISHERS_CERT_PATH, 0o700, exist_ok=True)
-        self._create_cert_files_if_needed(AAR_SERVER_CERT_PATH, AAR_SERVER_KEY_PATH)
+        pki.create_cert_files_if_needed(
+            cert_path=AAR_SERVER_CERT_PATH,
+            key_path=AAR_SERVER_KEY_PATH,
+            hostname=self.public_ip,
+            public_ip=self.public_ip,
+            private_ip=self.private_ip
+        )
+
+    def _on_config_changed(self, event: ops.ConfigChangedEvent):
+        self.unit.status = ops.MaintenanceStatus("Configuring AAR")
+        if not self._is_snap_installed(SNAP_NAME):
+            self.unit.status = ops.MaintenanceStatus("Installing AAR")
+            self._install_snap()
+            self._setup_certs()
+
+        port = ops.Port(protocol="tcp", port=int(self.config["port"]))
+        self._set_aar_config(port, self.private_ip)
+        self.unit.set_ports(port)
+
+        self._on_nrpe_available(event)
+
+        aar_snap = snap.SnapCache()['aar']
+        aar_snap.restart()
 
         version = self._get_snap_version(SNAP_NAME)
         self.unit.set_workload_version(version)
+
         self.unit.status = ops.ActiveStatus()
 
-    def _get_snap_version(self, snap_name: str) -> str:
-        snap_client = snap.SnapClient()
-        snaps = snap_client.get_installed_snaps()
-        for installed_snap in snaps:
-            if installed_snap["name"] == snap_name:
-                return installed_snap["version"]
-        return ""
-
-    def _on_config_changed(self, event: ops.ConfigChangedEvent):
-        if not self._snap_installed(SNAP_NAME):
-            self._on_install(event)
-            return
-        port = ops.Port(protocol="tcp", port=int(self.config["port"]))
-        listen_address = self.private_ip
+    def _set_aar_config(self, port: ops.Port, listen_address: str):
         tenv = Environment(loader=FileSystemLoader("templates"))
         template = tenv.get_template("config.yaml.j2")
         rendered_content = template.render(
@@ -283,11 +129,120 @@ class AARCharm(ops.CharmBase):
             }
         )
         AAR_CONFIG_PATH.write_text(rendered_content)
-        self.unit.set_ports(port)
-        systemd.service_restart(SERVICE)
 
+    def _on_stop(self, _: ops.StopEvent):
+        snap.remove('aar')
+
+    def _on_aar_joined(self, event: ops.RelationJoinedEvent):
+        with open(AAR_SERVER_CERT_PATH, "r") as f:
+            cert = f.read()
+        if not cert:
+            self.unit.status = ops.BlockedStatus("No registry certificate")
+            return
+
+        listen_address = self.private_ip
+        location = self.config.get("location")
+        if location:
+            listen_address = location
+
+        unit_data = event.relation.data[self.unit]
+        unit_data["certificate"] = cert
+        unit_data["fingerprint"] = pki.get_fingerprint(cert)
+        unit_data["ip"] = listen_address
+        unit_data["port"] = str(self.config.get("port", ""))
+
+    def _on_aar_changed(self, event: ops.RelationChangedEvent):
+        self.unit.status = ops.MaintenanceStatus("Confguring new AAR Client")
+        self._remove_all_certificates()
+        ams_clients = []
+        for unit in event.relation.units:
+            cert = event.relation.data[unit].get("certificate")
+            if not cert:
+                continue
+
+            # If there is a relation mismatch, we set an error status
+            # ex: juju add-relation aar:client ams:publisher
+            mode = event.relation.data[unit]["mode"].strip('"')
+            if mode not in event.relation.name:
+                self.unit.status = ops.BlockedStatus(
+                    "Invalid relation {} to {}".format("arr-" + mode, event.relation.name)
+                )
+                return
+            ams_clients.append(event.relation.data[unit])
+
+        if not ams_clients:
+            return
+        for client in ams_clients:
+            try:
+                self._register_aar_client(client["certificate"], event.relation.name)
+                logger.info(f"new client registered")
+            except CalledProcessError as ex:
+                logger.error(f"failed to add client to aar: {ex.output}")
+                self.unit.status = ops.BlockedStatus('Failed to register client certificate')
+                return
+
+        aar_snap = snap.SnapCache()["aar"]
+        aar_snap.restart()
         self.unit.status = ops.ActiveStatus()
 
+    def _on_nrpe_available(self, _):
+        if self.nrpe_client.is_available:
+            check_name = "check_{}".format(self.model.unit.name.replace("/", "_"))
+            self.nrpe_client.add_check(
+                command=[
+                    '/usr/lib/nagios/plugins/check_http',
+                    '--ssl', '--hostname', "{}:{}".format(self.private_ip, self.config["port"]),
+                    '--url', '/1.0/status', '--expect', '200',
+                    '--warning', '5', '--critical', '10'
+                ],
+                name=check_name,
+            )
+            self.nrpe_client.commit()
+
+    def _remove_all_certificates(self):
+        """Remove all old certificates held by AAR.
+
+        All client certificates are kept by juju in the relation until the hook
+        fails, so every time a new client is registered, the previous certificates
+        will be added again. To avoid duplicates and properly take into account
+        units that departed, we remove all certificates and add the ones that
+        are still active again.
+        """
+        shutil.rmtree(CLIENT_CERT_PATH)
+        shutil.rmtree(PUBLISHERS_CERT_PATH)
+        os.makedirs(CLIENT_CERT_PATH, 0o700, exist_ok=True)
+        os.makedirs(PUBLISHERS_CERT_PATH, 0o700, exist_ok=True)
+
+    def _register_aar_client(self, client_certificate: str, mode: str):
+        client_certificate = client_certificate.replace("\\n", "\n").strip('"')
+        cmd = ["/snap/bin/aar", "trust", "add"]
+        if mode == "publisher":
+            cmd.append("--publisher")
+        check_output(cmd, stderr=STDOUT, input=client_certificate.encode("utf-8"))
+
+    def _is_snap_installed(self, snap_name) -> bool:
+        snap_client = snap.SnapClient()
+        snaps = snap_client.get_installed_snaps()
+        for installed_snap in snaps:
+            if installed_snap["name"] == snap_name:
+                return True
+        return False
+
+    def _get_ip_for_interface(self, interface):
+        """Return the ip address associated to the given interface."""
+        addresses = netifaces.ifaddresses(interface)
+        if netifaces.AF_INET not in addresses or "addr" not in addresses[netifaces.AF_INET][0]:
+            raise Exception("No IP associated to requested device")
+
+        return addresses[netifaces.AF_INET][0]["addr"]
+
+    def _get_snap_version(self, snap_name: str) -> str:
+        snap_client = snap.SnapClient()
+        snaps = snap_client.get_installed_snaps()
+        for installed_snap in snaps:
+            if installed_snap["name"] == snap_name:
+                return installed_snap["version"]
+        return ""
 
 if __name__ == "__main__":
     ops.main(AARCharm)
---
+++ src/helpers/pki.py
@@ -0,0 +1,83 @@
+from datetime import datetime, timedelta
+import ipaddress
+import os
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+def get_fingerprint(cert: str) -> str:
+    x509_cert = x509.load_pem_x509_certificate(cert.encode())
+    return x509_cert.fingerprint(hashes.SHA256()).hex()
+
+def create_cert_files_if_needed(cert_path, key_path, hostname: str, public_ip: str, private_ip: str):
+    if os.path.exists(cert_path) and os.path.exists(key_path):
+        return
+
+    cert, key = _generate_selfsigned_cert(hostname, public_ip, private_ip)
+
+    cert_base_path = os.path.dirname(cert_path)
+    if not os.path.exists(cert_base_path):
+        os.makedirs(cert_base_path, mode=0o0700)
+
+    key_base_path = os.path.dirname(key_path)
+    if not os.path.exists(key_base_path):
+        os.makedirs(key_base_path, mode=0o0700)
+
+    with open(os.open(cert_path, os.O_CREAT | os.O_WRONLY, 0o600), "w") as f:
+        f.write(str(cert, "UTF-8"))
+
+    with open(os.open(key_path, os.O_CREAT | os.O_WRONLY, 0o600), "w") as f:
+        f.write(str(key, "UTF-8"))
+
+def _generate_selfsigned_cert(hostname, public_ip, private_ip):
+    if not hostname:
+        raise Exception("A hostname is required")
+
+    if not public_ip:
+        raise Exception("A public IP is required")
+
+    if not private_ip:
+        raise Exception("A private IP is required")
+
+    key = rsa.generate_private_key(
+        public_exponent=65537, key_size=4096, backend=default_backend()
+    )
+
+    name = x509.Name([x509.NameAttribute(x509.NameOID.COMMON_NAME, hostname)])
+    alt_names = x509.SubjectAlternativeName(
+        [
+            # best practice seem to be to include the hostname in the SAN
+            x509.DNSName(hostname),
+            # allow addressing by IP, for when you don't have real DNS
+            # openssl wants DNSnames for ips...
+            x509.DNSName(public_ip),
+            x509.DNSName(private_ip),
+            x509.IPAddress(ipaddress.IPv4Address(public_ip)),
+            x509.IPAddress(ipaddress.IPv4Address(private_ip)),
+        ]
+    )
+    # path_len=0 means this cert can only sign itnot other certs.
+    basic_contraints = x509.BasicConstraints(ca=True, path_length=0)
+    now = datetime.utcnow()
+    cert = (
+        x509.CertificateBuilder()
+        .subject_name(name)
+        .issuer_name(name)
+        .public_key(key.public_key())
+        .serial_number(1000)
+        .not_valid_before(now)
+        .not_valid_after(now + timedelta(days=10 * 365))
+        .add_extension(basic_contraints, False)
+        .add_extension(alt_names, False)
+        .sign(key, hashes.SHA256(), default_backend())
+    )
+    cert_pem = cert.public_bytes(encoding=serialization.Encoding.PEM)
+    key_pem = key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+
+    return cert_pem, key_pem
+
